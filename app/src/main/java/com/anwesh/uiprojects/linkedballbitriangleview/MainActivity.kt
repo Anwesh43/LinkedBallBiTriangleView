c0e61cd6d4398019218e7900b070e46d580133e0
Anwesh43 <anweshthecool0@gmail.com>
2019-06-02 14:14:37 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedballbitriangleview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedballbitriangleview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedballbitriangleview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.ballbitriangleview.BallBiTriangleView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         BallBiTriangleView.create(this)
+        fullScreen()
     }
+}
+
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
